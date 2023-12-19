9696ff13231957055114151f7d7e3237650a91e3
João Aguiar <macdjoao@gmail.com>
2023-12-19 17:56:06 -0300

    feat: Model para Clock

--- clock/models.py
+++ clock/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Clock(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    check_in_date = models.DateField(input_formats=['%Y-%m-%d'])
+    check_out_date = models.DateField(input_formats=['%Y-%m-%d'])
+
+    check_in_hour = models.TimeField(input_formats=['%H:%M'])
+    check_out_hour = models.TimeField(input_formats=['%H:%M'])
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = "Clock"
+        verbose_name_plural = "Clocks"
+
+    def __str__(self):
+        return f"{self.user.username} - {self.check_in_date}/{self.check_in_hour.strftime('%H:%M')} - {self.check_out_date}/{self.check_out_hour.strftime('%H:%M')}"
