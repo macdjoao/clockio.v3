de084c4d76c8e91c61ff8947d821da925165dcac
João Aguiar <macdjoao@gmail.com>
2023-12-19 18:07:21 -0300

    config: Adicionando model Clock ao django admin

--- clock/admin.py
+++ clock/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from clock import models
 
-# Register your models here.
+
+@admin.register(models.Clock)
+class ClockAdmin(admin.ModelAdmin):
+    list_display = ['user', 'check_in_hour', 'check_out_hour']
+    search_fields = ['user', 'check_in_date', 'check_out_date']
+    list_filter = ['user']
