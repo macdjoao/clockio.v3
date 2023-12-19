a7931105a7ecfa3d1727d78746da387d53ed0946
João Aguiar <macdjoao@gmail.com>
2023-12-19 18:06:59 -0300

    fix? Correção dos campos do model Clock

--- clock/models.py
+++ clock/models.py
@@ -5,18 +5,20 @@ from django.contrib.auth.models import User
 class Clock(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='created_clocks')
 
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='updated_clocks')
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    check_in_date = models.DateField(input_formats=['%Y-%m-%d'])
-    check_out_date = models.DateField(input_formats=['%Y-%m-%d'])
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
 
-    check_in_hour = models.TimeField(input_formats=['%H:%M'])
-    check_out_hour = models.TimeField(input_formats=['%H:%M'])
+    check_in_hour = models.TimeField()
+    check_out_hour = models.TimeField()
 
     class Meta:
         ordering = ['-id']
