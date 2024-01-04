d8a731a77703546338b9e7c86821a7957aea2fe2
João Aguiar <macdjoao@gmail.com>
2024-01-04 17:59:25 -0300

    feat/refact: Criação de UpdateClockSerializer e refatoração de CreateClockSerializer

--- clock/serializers.py
+++ clock/serializers.py
@@ -6,19 +6,21 @@ from clock.models import Clock
 
 
 class CreateClockSerializer(serializers.ModelSerializer):
+    check_in = serializers.DateTimeField(required=True)
+    check_out = serializers.DateTimeField(required=True)
+
     class Meta:
         model = Clock
         fields = ['check_in', 'check_out']
-        extra_kwargs = {'check_in': {'required': True},
-                        'check_out': {'required': True}}
 
     def validate(self, data):
-        check_in = data.get('check_in')
-        check_out = data.get('check_out')
+        check_in = data['check_in']
+        check_out = data['check_out']
 
         if check_out <= check_in:
             raise serializers.ValidationError(
-                'check_out field must be a date more recent than check_in field')
+                {'validation_error': 'check_out field must be a date more recent than check_in field'})
+
         return data
 
 
@@ -27,3 +29,35 @@ class ReadClockSerializer(serializers.ModelSerializer):
         model = Clock
         fields = ['id', 'created_at', 'created_by', 'updated_at',
                   'updated_by', 'check_in', 'check_out']
+
+
+class UpdateClockSerializer(serializers.ModelSerializer):
+    check_in = serializers.DateTimeField(required=False)
+    check_out = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = Clock
+        fields = ['check_in', 'check_out']
+
+    def validate(self, data):
+        check_in = data.get('check_in')
+        check_out = data.get('check_out')
+        instance = self.instance
+
+        if not check_in and not check_out:
+            raise serializers.ValidationError(
+                {'validation_error': 'at least check_in or check_out must be received'})
+
+        if check_in and check_out and check_out <= check_in:
+            raise serializers.ValidationError(
+                {'validation_error': 'check_out field must be a date more recent than check_in field'})
+
+        if not check_out and check_in and check_in < instance.check_out:
+            raise serializers.ValidationError(
+                {'check_in': 'check_in field must be a date more recent than the current check_out field'})
+
+        if not check_in and check_out and check_out < instance.check_in:
+            raise serializers.ValidationError(
+                {'check_out': 'check_out field must be a date older than the current check_in field'})
+
+        return data
